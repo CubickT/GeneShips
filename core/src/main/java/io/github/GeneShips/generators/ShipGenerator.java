9cfa22799462248f609deec092e41e4a7b54244a
Cub <towncubick@gmail.com>
2025-10-21 21:21:40 +0700

    refactor: ShipGenerator makes more ship-shaped vertices
    refactor: ship rendering - implement triangulated hulls with color filling

--- core/src/main/java/io/github/GeneShips/generators/ShipGenerator.java
+++ core/src/main/java/io/github/GeneShips/generators/ShipGenerator.java
@@ -13,43 +13,36 @@ public class ShipGenerator {
         return enRangeS + (enRangeE - enRangeS) * ((value - rangeS) / (rangeE - rangeS));
     }
 
-    public void generateShipAt(float x, float y, ShapeRenderer shapeRenderer) {
-
-
-        renderShip(x, y, length, width, bowAngle, shapeRenderer);
-    }
-
     public void renderShip(float x, float y, float length, float width, float bowAngle, ShapeRenderer shapeRenderer) {
-        float noseWidth = map(bowAngle, 0, 1, width * 1.7F, width * 0.6F);
+        float noseWidth = map(bowAngle, 0, 1, width * 1F, width * 0.6F);
         float noseLength = map(bowAngle, 0, 1, 40, 100);
 
         float[][] vertices = {
-            {0, 0},                            // Нос
+            {0.5F * length + noseLength, 0},                            // Нос
             // Правый борт
-            {-noseLength * 0.5F, noseWidth},     // Правый угол носа
-            {-noseLength, width},              // Правый передний угол корпуса
-            {-length * 0.8F, width},                  // Правый задний угол корпуса
+            {0.5F * length + 0.5F * noseLength, noseWidth},     // Правый угол носа
+            {0.5F * length, width},              // Правый передний угол корпуса
+            {-0.4F * length * 0.8F, width},                  // Правый задний угол корпуса
             // Корма
-            {-length, width * 0.7F},          // Правый угол кормы
-            {-length, -width * 0.7F},         // Левый угол кормы
+            {-0.5F * length, width * 0.7F},          // Правый угол кормы
+            {-0.5F * length, -width * 0.7F},         // Левый угол кормы
             //Левый борт
-            {-length * 0.8F, -width},                  // Левый задний угол корпуса
-            {-noseLength, -width},                // Левый передний угол корпуса
-            {-noseLength * 0.5F, -noseWidth},   // Левый угол носа
+            {-0.4F * length * 0.8F, -width},                  // Левый задний угол корпуса
+            {0.5F * length, -width},                // Левый передний угол корпуса
+            {0.5F * length + 0.5F * noseLength, -noseWidth},   // Левый угол носа
         };
 
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.setColor(Color.LIGHT_GRAY);
+        shapeRenderer.setColor(Color.RED);
 
-        for (int i = 0; i < vertices.length; i++) {
-            int next = (i + 1) % vertices.length;
-            float x1 = x + vertices[i][0];
-            float y1 = y + vertices[i][1];
-            float x2 = x + vertices[next][0];
-            float y2 = y + vertices[next][1];
-
-            shapeRenderer.line(x1, y1, x2, y2);
+        for (int i = 1; i < vertices.length - 1; i++) {
+            shapeRenderer.triangle(
+                x + vertices[0][0], y + vertices[0][1],  // Центральная точка (нос)
+                x + vertices[i][0], y + vertices[i][1],   // Текущая вершина
+                x + vertices[i + 1][0], y + vertices[i + 1][1]  // Следующая вершина
+            );
         }
+
         shapeRenderer.end();
     }
 }
